acfbdcfff28e3fef4faf720d6c41d75739345ca5
Kim Barry <kimbarrymail@gmail.com>
2022-10-06 14:13:22 -0400

    dispaly rough draft

--- capstone/Capstone/Models/Display.cs
+++ capstone/Capstone/Models/Display.cs
@@ -6,12 +6,19 @@ namespace Capstone.Models
 {
     public class Display
     {
-
+        //create a "main menu" method
+        //create a "purchase menu" method
+        //create text scripting for each choice
+        Attribute 
+        int choiceFromMainMenu = 0;
         public string SelectedItem { get; private set; }
 
 
         public string Start() //read file; display all items; pick item; pay item 
         {
+            //read the inventory text file:
+            // set choiceFromMainMenu to 0
+            //call MainMenu()
 
         }
 
@@ -24,7 +31,25 @@ namespace Capstone.Models
         {
 
         }
-
+        public void MainMenu()
+        {
+            Console.WriteLine("***   Vend-O-Matic   ***");
+            Console.WriteLine("***                  ***");
+            Console.WriteLine("Welcome To The Main Menu");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Please choose from the following options:");
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("Enter 1 to display available snacks");
+            Console.WriteLine("Enter 2 to make a purchase");
+            Console.WriteLine("Enter 3 to exit Vend-O-Matic");
+            choiceFromMainMenu = int.Parse(Console.ReadLine());
+            // here the readline will create a variable that parses to be our choice
+            //1 dispaly items
+            //2 make purchase
+            //3 exit
+        }
 
     }
 }
